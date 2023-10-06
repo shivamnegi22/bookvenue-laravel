e7b16fa5c73588b2fafb071680c89852f48ed2e3
Saurabh135790 <saurabh.panwar@giksindia.com>
2023-10-06 17:58:46 +0530

    add accordion on sport

--- resources/views/facility/allSports.blade.php
+++ resources/views/facility/allSports.blade.php
@@ -1,1 +1,47 @@
 @extends('layouts.aside')
+@section('content')
+
+<table id="myTable" class="display table responsive nowrap" width="100%">
+    <thead>
+        <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+            <th>Column 4</th>
+            <th>Column 5</th>
+            <th>Column 6</th>
+            <th>Column 7</th>
+            <th>Column 8</th>
+            <th>Column 9</th>
+            <th>Column 10</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
+</table>
+
+@endsection
--- resources/views/facility/facilitySport.blade.php
+++ resources/views/facility/facilitySport.blade.php
@@ -17,10 +17,10 @@
             <div class="col-md-6">
                 <label>Sport</label>
                 <select class="inputField" name="sports_id">
-                <option value="By the way">Choose Sport</option>
-                @foreach($sports as $sport)
+                    <option value="By the way">Choose Sport</option>
+                    @foreach($sports as $sport)
                     <option value="{{$sport->id}}">{{ $sport->name }}</option>
-                @endforeach
+                    @endforeach
                 </select>
             </div>
             <div class="col-md-4">
@@ -37,16 +37,25 @@
             </div>
             <div class="col-md-6">
                 <label>Slot Time</label>
-                <input type="text" name="slot_time" placeholder="slot_time" class="inputField">
+                <input type="text" name="slot_time" placeholder="Slot Time" class="inputField">
             </div>
             <div class="col-md-6">
                 <label>Holiday</label>
-                <input type="text" name="holiday[]" placeholder="slot_time" class="form-control-file">
+                <input type="text" name="holiday[]" placeholder="Holiday" class="inputField">
             </div>
             <div class="col-md-12">
                 <label>Description</label>
                 <textarea type="text" name="description" placeholder="Description" class="inputField"></textarea>
             </div>
+            <div class="col-md-12 mb-3">
+                <span id="addAccordionItem" class="formButton add">Add New Accordion Item</span>
+            </div>
+            <div class="col-md-12">
+                <div class="accordion" id="accordionExample">
+
+                </div>
+            </div>
+
             <div class="col-md-12">
                 <button type="submit" class="formButton submit" name="submit">Save</button>
             </div>
@@ -54,4 +63,58 @@
     </div>
 </form>
 
+
+<script>
+function createAccordionItem(index) {
+    const newItem = `
+                <div class="card">
+                    <div class="card-header" id="heading${index}">
+                        <h5 class="mb-0">
+                            <button class="btn collapsed" type="button" data-toggle="collapse"
+                                data-target="#collapse${index}" aria-expanded="false" aria-controls="collapse${index}">
+                                Collapsible Group Item #${index}
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}"
+                        data-parent="#accordionExample">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Name</label>
+                                    <input type="text" name="name" placeholder="Name" class="inputField">
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Slot Price</label>
+                                    <input type="text" name="slot_price" placeholder="Slot Price" class="inputField">
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Breaktime Start</label>
+                                    <input type="time" name="breaktime_start" placeholder="Breaktime Start" class="inputField">
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Breaktime End</label>
+                                    <input type="time" name="breaktime_end" placeholder="Breaktime End" class="inputField">
+                                </div>
+                                <div class="col-md-12">
+                                    <label>Description</label>
+                                    <textarea type="text" name="description" placeholder="Description" class="inputField"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+    return newItem;
+}
+
+document.getElementById('addAccordionItem').addEventListener('click', function() {
+    const accordionContainer = document.querySelector('.accordion');
+    const newItemIndex = accordionContainer.children.length + 1;
+    const newItemHTML = createAccordionItem(newItemIndex);
+
+    const newItemElement = document.createRange().createContextualFragment(newItemHTML);
+    accordionContainer.appendChild(newItemElement);
+});
+</script>
 @endsection
