f7bc999948349496e1f6a25916069e8e881e9476
Saurabh0721 <saurabh.negi@giksindia.com>
2023-10-27 15:26:51 +0530

    update validation of add services

--- resources/views/facility/addServices.blade.php
+++ resources/views/facility/addServices.blade.php
@@ -66,24 +66,33 @@
                         <label>End Time</label>
                         <input type="time" class="inputField" name="endTime[]" id="endTime" required/>
                         <span class="text-danger d-none" id="endTimeError">required</span>
+                        <span class="text-danger d-none" id="endTimeValError">Invalid End Time (atleast 15min > start time)</span>
                     </div>
-                    <div class="col-md-4">
-                        <label>Price</label>
-                        <input type="number" class="inputField" name="price[]" id="price" required/>
-                        <span class="text-danger d-none" id="priceError">required</span>
+                    <div class="col-md-4 hideForm d-none">
+                        <label>Description</label>
+                        <textarea class="inputField h-auto" rows="5" name="court_description[]" placeholder="description"></textarea>
                     </div>
-                    <div class="col-md-4">
-                        <label>Duration</label>
-                        <input type="number" class="inputField" name="duration[]" id="duration" required/>
-                        <span class="text-danger d-none" id="durationError">required</span>
-                    </div>
-                    <div class="col-md-4">
-                        <label>Break Start</label>
-                        <input type="time" class="inputField" name="break_start[]" />
-                    </div>
-                    <div class="col-md-4">
-                        <label>Break End</label>
-                        <input type="time" class="inputField" name="break_end[]" />
+                    <div class="col-md-8 hideForm d-none">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Price</label>
+                                <input type="number" class="inputField" name="price[]" id="price" required/>
+                                <span class="text-danger d-none" id="priceError">required</span>
+                            </div>
+                            <div class="col-md-6">
+                                <label>Duration</label>
+                                <input type="number" class="inputField" name="duration[]" id="duration" required/>
+                                <span class="text-danger d-none" id="durationError">required</span>
+                            </div>
+                            <div class="col-md-6">
+                                <label>Break Start</label>
+                                <input type="time" class="inputField" id="breakStart" name="break_start[]" />
+                            </div>
+                            <div class="col-md-6">
+                                <label>Break End</label>
+                                <input type="time" class="inputField" id="breakEnd" name="break_end[]" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -100,8 +109,7 @@
 @endsection
 
 @section('script')
-
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="{{asset('assest/js/addService.js')}}"></script>
+<script src="{{asset('assest/js/addService.js')}}" defer></script>
 
 @endsection
